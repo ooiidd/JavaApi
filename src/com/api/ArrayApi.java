09312bb208e7d0fa6adbbdcef2a5d45844e49707
SongHun <ooiidd@naver.com>
2020-04-14 19:43:24 +0900

    Array Api 추가

--- src/com/api/ArrayApi.java
+++ src/com/api/ArrayApi.java
@@ -1,5 +1,17 @@
 package com.api;
 
-public class ArrayApi {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class ArrayApi {
+	public static <T> List<T> arrayToList(T[] arr) {
+		return new ArrayList<T>(Arrays.asList(arr));
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T[] listToArray(List<T> list) {
+		return list.toArray((T[])new Object[list.size()]);
+	}
 }
