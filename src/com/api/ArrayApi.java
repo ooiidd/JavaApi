021c49bae4e2d3b57687d736164780ae982e6c46
SongHun <ooiidd@naver.com>
2020-04-14 20:45:44 +0900

    Array to List / List to Array 추가

--- src/com/api/ArrayApi.java
+++ src/com/api/ArrayApi.java
@@ -1,17 +1,48 @@
 package com.api;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayApi {
 	public static <T> List<T> arrayToList(T[] arr) {
 		return new ArrayList<T>(Arrays.asList(arr));
 	}
 	
+	public static List<Integer> arrayToList(int[] arr) {
+		return Arrays.stream(arr).boxed().collect(Collectors.toList());
+	}
+	
+	public static List<List<Integer>> arrayToList(int[][] arr) {
+		List<List<Integer>> list = new ArrayList<>();
+		for(int i=0;i<arr.length;i++) {
+			list.add(arrayToList(arr[i]));
+		}
+		return list;
+	}
+	
+	
+	public static <T> List<List<T>> arrayToList(T[][] arr) {
+		List<List<T>> list = new ArrayList<>();
+		for(int i=0;i<arr.length;i++) {
+			list.add(arrayToList(arr[i]));
+		}
+		return list;
+	}
+	
 	@SuppressWarnings("unchecked")
 	public static <T> T[] listToArray(List<T> list) {
+		T[] arr = list.toArray((T[])new Object[list.size()]);
 		return list.toArray((T[])new Object[list.size()]);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T[][] listToDoubleArray(List<List<T>> list) {
+		Object[][] arr = new Object[list.size()][]; 
+		for(int i=0;i<list.size();i++) {
+			arr[i] = listToArray(list.get(i));
+		}
+		return (T[][])arr;
 	}
 }
--- src/com/test/ArrayApiTest.java
+++ src/com/test/ArrayApiTest.java
@@ -1,7 +1,75 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.api.ArrayApi;
+
 public class ArrayApiTest {
 	public static void main(String[] args) {
+		int[][] doubleArray = {
+				{1,2,3},
+				{2,3,4},
+				{3,4,5}
+		};
+		int[] array = {
+				1,2,3,4,5
+		};
 		
+		String[][] strDoubleArray = {
+				{"1bc","2","3"},
+				{"2","3","4"},
+				{"3","4","5"}
+		};
+		String[] strArray = {
+				"1a","2","3","4","5"
+		};
+		
+		List<Integer> list = new ArrayList<>(Arrays.asList(10,9,8,7,6));
+		List<String> strList = new ArrayList<>(Arrays.asList("abcd","9","8","7","6"));
+		List<List<Integer>> doubleList = new ArrayList<>(Arrays.asList(
+				Arrays.asList(10,9,8,7),
+				Arrays.asList(6,5,4),
+				Arrays.asList(3,2,1)
+		));
+		List<List<String>> strDoubleList = new ArrayList<>(Arrays.asList(
+				Arrays.asList("ab","9","8","7"),
+				Arrays.asList("6","5","4"),
+				Arrays.asList("3","2","1")
+		));
+		System.out.println("------------------(array -> List)int 변환-------------------");
+		
+		System.out.println(ArrayApi.arrayToList(array));
+		System.out.println(  ArrayApi.arrayToList(doubleArray).stream().map(
+				t -> {
+					System.out.println(t);
+					return t.toString();
+				}
+			).collect(Collectors.toList())   );
+		System.out.println("--------------------------------------------");
+		
+		System.out.println("------------------(array -> List)String 변환-------------------");
+		System.out.println( ArrayApi.arrayToList(strArray));
+		System.out.println(  ArrayApi.arrayToList(strDoubleArray).stream().map(
+				t -> {
+					System.out.println(t);
+					return t.toString();
+				}
+			).collect(Collectors.toList())  );
+		System.out.println("--------------------------------------------");
+		
+
+		System.out.println("------------------(List -> array)int 변환-------------------");
+		System.out.println( Arrays.deepToString( ArrayApi.listToArray(list)) );
+		System.out.println( Arrays.deepToString( ArrayApi.listToDoubleArray(doubleList)) );
+		System.out.println("--------------------------------------------");
+
+		System.out.println("------------------(List -> array)String 변환-------------------");
+		System.out.println( Arrays.deepToString( ArrayApi.listToArray(strList)) );
+		System.out.println( Arrays.deepToString( ArrayApi.listToDoubleArray(strDoubleList)) );
+	
+		System.out.println("--------------------------------------------");
 	}
 }
