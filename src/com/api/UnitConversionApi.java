c53e172a3e0663637f36be2e7a1828d555798a7e
SongHun <ooiidd@naver.com>
2020-04-15 15:12:33 +0900

    Convert Util 추가

--- src/com/api/UnitConversionApi.java
+++ src/com/api/UnitConversionApi.java
@@ -1,5 +1,63 @@
 package com.api;
 
-public class UnitConversionApi {
+import javax.xml.ws.BindingType;
 
+public class UnitConversionApi {
+	
+	
+	/**
+	 * m/s -> km/s
+	 * @param speed
+	 * @return
+	 */
+	public static double meterPerSecToKmPerSec(int speed) {
+		double input = Double.valueOf(speed);
+		return input/1000.0; 
+	}
+	public static double meterPerSecToKmPerSec(double speed) {
+		double input = Double.valueOf(speed);
+		return input/1000.0; 
+	}
+	public static double meterPerSecToKmPerSec(float speed) {
+		double input = Double.valueOf(speed);
+		return input/1000.0; 
+	}
+	
+	/**
+	 * m/s -> km/h
+	 * @param speed
+	 * @return
+	 */
+	public static double meterPerSecToKmPerHour (int speed) {
+		double input = Double.valueOf(speed);
+		return input*3.6;
+	}
+	public static double meterPerSecToKmPerHour (double speed) {
+		double input = Double.valueOf(speed);
+		return input*3.6;
+	}
+	public static double meterPerSecToKmPerHour (float speed) {
+		double input = Double.valueOf(speed);
+		return input*3.6;
+	}
+	
+	/**
+	 * km/h -> m/s 
+	 * @param speed
+	 * @return
+	 */
+	public static double kmPerHourToMeterPerSec (int speed) {
+		double input = Double.valueOf(speed);
+		return input/3.6;
+	}
+	public static double kmPerHourToMeterPerSec (double speed) {
+		double input = Double.valueOf(speed);
+		return input/3.6;
+	}
+	public static double kmPerHourToMeterPerSec (float speed) {
+		double input = Double.valueOf(speed);
+		return input/3.6;
+	}
+	
+	
 }
--- src/com/test/SortApiTest.java
+++ src/com/test/SortApiTest.java
@@ -1,17 +1,27 @@
 package com.test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import com.api.SortApi;
 
 public class SortApiTest {
 
 	public static void main(String[] args) {
-		int[] parr = {3,1,2,4,5};
+		int[] intArray = {3,1,2,4,5};
+		int[] parr = intArray.clone();
 		parr = SortApi.sort1(parr); for(int el : parr) {System.out.print(el + " ");} System.out.println();
+		
+		parr = intArray.clone();
 		parr = SortApi.sort2(parr); for(int el : parr) {System.out.print(el + " ");} System.out.println();
 		
 		String[] sarr = {"acneStudio", "macbook", "apple"};
 		sarr = SortApi.sort3(sarr); for(String el : sarr) {System.out.print(el + " ");} System.out.println();
 		sarr = SortApi.sort4(sarr); for(String el : sarr) {System.out.print(el + " ");} System.out.println();
+		
+
+		Integer[] Iparr = intArray.clone();
+		Arrays.sort(parr,Collections.reverseOrder());
 	}
 
 }
