c6926d83fc065c492ad593d79a73dea8a21aa67c
Song <stalker5217@gmail.com>
2020-04-15 13:39:33 +0900

    gg요

--- src/com/api/SortApi.java
+++ src/com/api/SortApi.java
@@ -1,5 +1,51 @@
 package com.api;
 
-public class SortApi {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class SortApi {
+	
+	/**
+	 * primitive type 오름차순 정렬
+	 * @param input : primitive integer array
+	 * @return sort result
+	 */
+	public static int[] sort1(int[] input) {
+		Arrays.sort(input);
+		return input;
+	}
+	
+	/**
+	 * primitive type 내림차순
+	 * 내림차순은 깝치지 말고 wrapper class array나 collection에 담아서 쓰도록 하자.
+	 * @param input : primitive integer array
+	 * @return
+	 */
+	public static int[] sort2(int[] input) {
+		List<Integer> wrapInput = ArrayApi.arrayToList(input); 
+		Collections.sort(wrapInput, Collections.reverseOrder());		
+		input = wrapInput.stream().mapToInt(n->n).toArray();
+		return input;
+	}
+	
+	/**
+	 * string 오름차순
+	 * @param input : string array
+	 * @return
+	 */
+	public static String[] sort3(String[] input) {
+		Arrays.sort(input);
+		return input;
+	}
+	
+	/**
+	 * string 내림차순
+	 * @param input : string array
+	 * @return
+	 */
+	public static String[] sort4(String[] input) {
+		Arrays.sort(input, Collections.reverseOrder());
+		return input;
+	}
 }
--- src/com/test/SortApiTest.java
+++ src/com/test/SortApiTest.java
@@ -1,9 +1,17 @@
 package com.test;
 
+import com.api.SortApi;
+
 public class SortApiTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] parr = {3,1,2,4,5};
+		parr = SortApi.sort1(parr); for(int el : parr) {System.out.print(el + " ");} System.out.println();
+		parr = SortApi.sort2(parr); for(int el : parr) {System.out.print(el + " ");} System.out.println();
+		
+		String[] sarr = {"acneStudio", "macbook", "apple"};
+		sarr = SortApi.sort3(sarr); for(String el : sarr) {System.out.print(el + " ");} System.out.println();
+		sarr = SortApi.sort4(sarr); for(String el : sarr) {System.out.print(el + " ");} System.out.println();
 	}
 
 }
